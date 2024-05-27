e123b36ecc8e2230635441f7853f38b6dc37a0a8
Dan Li <danli091981@gmail.com>
2024-05-26 22:28:14 -0400

    Create chinese.vue

---
+++ src/views/language/chinese.vue
@@ -0,0 +1,25 @@
+<template>
+    <Navbar />
+
+    <div>
+        <h2>Chinese page</h2>
+    </div>
+
+    <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+    name: 'Chinese',
+    components: {
+        Navbar, Footer
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
