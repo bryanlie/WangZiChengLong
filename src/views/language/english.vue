fcb1fe674744555faf7fba086c73c4c12164338f
Dan Li <danli091981@gmail.com>
2024-05-26 22:28:29 -0400

    Add files via upload

---
+++ src/views/language/english.vue
@@ -0,0 +1,25 @@
+<template>
+    <Navbar />
+
+    <div>
+        <h2>English page</h2>
+    </div>
+
+    <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+    name: 'English',
+    components: {
+        Navbar, Footer
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
