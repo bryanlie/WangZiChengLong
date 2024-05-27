dfe1d55753bf1b4a89c111b13593f387eb0d1c3a
Dan Li <danli091981@gmail.com>
2024-05-26 22:27:34 -0400

    Create violin.vue

---
+++ src/views/music/violin.vue
@@ -0,0 +1,25 @@
+<template>
+    <Navbar />
+
+    <div>
+        <h2>violin page</h2>
+    </div>
+
+    <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+    name: 'Violin',
+    components: {
+        Navbar, Footer
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
