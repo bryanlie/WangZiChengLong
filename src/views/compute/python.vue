8bb57a88a139f787d053a07e7f76d205d103f8c9
Dan Li <danli091981@gmail.com>
2024-05-26 22:27:02 -0400

    Create python.vue

---
+++ src/views/compute/python.vue
@@ -0,0 +1,26 @@
+<template>
+    <Navbar />
+
+     
+    <div>
+        <h2>python page</h2>
+    </div>
+
+    <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+    name: 'Python',
+    components: {
+        Navbar, Footer
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
