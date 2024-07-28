561f5e08f92798a122b4b4cd07b316daed55fbae
Dan Li <danli091981@gmail.com>
2024-07-27 21:56:00 -0400

    Update MathView.vue

--- src/views/MathView.vue
+++ src/views/MathView.vue
@@ -3,7 +3,6 @@
 
     <v-app>
   
-  
 
     <v-container class="my-4">
         <v-row>
@@ -62,30 +61,56 @@
                 <div>
                     <h2>Popular math competitions for elementary and middle school students</h2>
 
-                    <p><strong>Math Kangaroo:</strong>
+                    <p><strong> <a href="https://mathkangaroo.org/mks/" target="_blank">Math Kangaroo:</a></strong>
                         This worldwide competition focuses on fun and engaging math problems with multiple-choice answers. It caters to grades 1-12, 
-                        with separate levels for elementary school students.
+                        with separate levels for elementary school students. 
+                        Also see <a href="https://www.matematica.pt/en/useful/kangaroo-questions.php" target="_blank">Kangaroo Questions</a>
                     </p>
 
-                    <p><strong>National Trimathlon: </strong>
-                        This competition is designed for grades 3-6 and combines mental math, problem-solving, and speed rounds, making it a well-rounded challenge.
+                    <p><strong><a href="https://www.pimathcontest.com/" target="_blank">Pi Math Contest:</a> </strong>
+                        The Pi Math Contest (PiMC) is an annual mathematics competition organized by AlphaStar Academy for elementary, middle, and high school students.
+                        This contest allows students to participate individually and as part of a team. 
+                        It emphasizes creative problem-solving and celebrates the fascination with Pi.
+                        <v-row align="center">
+                            <v-col cols="12" md="2" class="pr-md-2">
+                                <v-img src="../assets/piMathContest.jpg" contain height="300"/>
+                            </v-col>
+                            <v-col cols="12" md="6" class="pl-md-2">
+                                <p>The Pi Math Contest (PiMC) books for Elementary School Contests are collections of past competition problems and solutions designed to help students prepare for the Pi Math Contest.
+                                    There are currently two volumes available for the Elementary School Contests. 
+                                    <a href="https://www.amazon.com/Math-Contest-Elementary-2015-2018-ALPHASTAR/dp/B09V27HFMP" target="_blank">Pi Math Contest Amazon book</a></p>
+                            </v-col>
+                        </v-row>
                     </p>
 
-                    <p><strong>Pi Math Contest (PiMC):</strong>
-                        This online competition offers two divisions for students up to grade 8. It emphasizes creative problem-solving and celebrates the fascination with pi.
+                    <p><strong><a href="https://moems.org/" target="_blank">Math Olympiads for Elementary and Middle Schools:</a></strong>
+                        Math Olympiads for Elementary and Middle Schools (MOEMS) is a popular mathematics competition for students in grades 4 through 8. 
+                        MOEMS consists of five monthly contests administered from November through March each year.
+                        Each contest includes five non-routine problems that require careful mathematical thinking.
+                        <v-row align="center">
+                            <v-col cols="12" md="2" class="pr-md-2">
+                                <v-img src="../assets/moems.jpg" contain height="300"/>
+                            </v-col>
+                            <v-col cols="12" md="6" class="pl-md-2">
+                                <p>This is a series of books that compile problems and solutions from the Math Olympiads for Elementary and Middle Schools (MOEMS) competitions. These books are valuable resources for students, teachers, and parents involved in mathematical problem-solving and olympiad preparation.
+                                    There are currently four volumes available in this series. 
+                                    <a href="https://www.amazon.com/Olympiad-Contest-Problems-Elementary-Schools/dp/0962666211" target="_blank">Math Olympiad Contest Problems Amazon book</a></p>
+                            </v-col>
+                        </v-row>
                     </p>
 
-                    <p><strong>Math League (various names):</strong>
-                        This is a league-based competition with different names depending on your region. It offers various levels for middle school students, 
-                        focusing on problem-solving skills.
+                    <p><strong>More competitions:</strong>
+                        There are numerous other math competitions at the elementary and middle school levels. <br>
+                        <a href="https://www.artofproblemsolving.com/wiki/index.php/List_of_United_States_elementary_school_mathematics_competitions" target="_blank">List of US Elementary School Math Competitions</a>
+                        <br> 
+                        <a href="https://www.artofproblemsolving.com/wiki/index.php/List_of_United_States_middle_school_mathematics_competitions" target="_blank">List of US Middle School Math Competitions</a>
                     </p>
-
-                    <p><strong>Noetic Learning Math Contest (NLMC):</strong>
-                        This semiannual competition is known for its challenging problems and encourages problem-solving approaches in grades 2-8.
-                    </p>
-
-                    <p><strong>Beestar National Competition:</strong>
-                        This online competition is the largest of its kind for grades 2-8, offering multiple difficulty levels and promoting problem-solving strategies.
+                    <p><strong>More resources:</strong>
+                         There are many resources available to help students prepare for math competitions. Some popular ones include:
+                        <br>
+                        <a href="https://www.khanacademy.org/" target="_blank">Khan Academy</a>
+                        <br>
+                        <a href="https://artofproblemsolving.com/wiki/index.php/Resources_for_mathematics_competitions" target="_blank">Resources for mathematics competitions</a>
                     </p>
                 </div>
             </v-col>
@@ -103,24 +128,34 @@
                 <div>
                     <h2>Popular math competitions can benefit college applications</h2>
 
-                    <p><strong>American Mathematics Competitions (AMC)</strong>
+                    <p><strong> <a href="https://maa.org/student-programs/amc/" target="_blank">American Mathematics Competitions (AMC):</a> </strong>
                         This is a multi-tiered competition with the AMC 10/12 for high school students. 
-                        Top scorers qualify for the next level, the American Invitational Mathematics Examination (AIME).
+                        Top scorers qualify for the next level, the <a href="https://artofproblemsolving.com/wiki/index.php/American_Invitational_Mathematics_Examination" target="_blank">American Invitational Mathematics Examination (AIME)</a>.
                     </p>
 
                     <p><strong>USA Mathematical Olympiad (USAMO)</strong>
                         This prestigious competition is the final stage of the AMC pipeline and is considered 
-                        a major achievement for prospective math students.
+                        a major achievement for prospective math students. 
+                        Top performers from AIME may qualify for the <a href="https://artofproblemsolving.com/wiki/index.php/United_States_of_America_Mathematical_Olympiad" target="_blank">USA Mathematical Olympiad (USAMO)</a> or USA Junior Mathematical Olympiad (USAJMO).
                     </p>
 
                     <p><strong>Math Olympiads (MO)</strong>
-                        The International Math Olympiad (IMO) and other regional Math Olympiads 
+                        The <a href="https://artofproblemsolving.com/wiki/index.php/International_Mathematical_Olympiad" target="_blank">international Mathematical Olympiad (IMO) </a> 
+                        and other regional Math Olympiads 
                         (like Asian Math Olympiad or European Girls' Mathematical Olympiad) are highly selective 
                         competitions for the absolute top math students globally.
                     </p>
 
-                    <p><strong>Harvard-MIT Mathematics Tournament (HMMT)</strong>
+                    <p><strong><a href="https://www.hmmt.org/" target="_blank">Mathematics Tournament (HMMT)</a></strong>
+                        The HMMT is a prestigious high school mathematics competition organized by students from Harvard University and the Massachusetts Institute of Technology (MIT)
                         This invitation-only team-based competition is known for its challenging problems and attracts top math students from around the world.
+                    </p>
+
+                    <p><strong>More competitions:</strong>
+                        There are numerous other math competitions at the regional, national, and international levels. 
+                        Participating in these competitions can help students develop problem-solving skills, 
+                        build a strong math foundation, and stand out in college applications. 
+                        <a href="https://www.artofproblemsolving.com/wiki/index.php/List_of_United_States_high_school_mathematics_competitions" target="_blank">List of US high school Math Competitions</a>
                     </p>
                 </div>
             </v-col>
@@ -140,7 +175,9 @@
 
                     <p><strong>AMC 8:</strong>
                         This 25-question, 40-minute multiple-choice exam is designed for students in grades 8 and below (under 14.5 years old). 
-                        It focuses on middle school math concepts.
+                        It focuses on middle school math concepts. For more details, visit the 
+                        <router-link to="/math/amc8">AMC 8 page</router-link>
+                        
                     </p>
 
                     <p><strong>AMC 10/12:</strong>
