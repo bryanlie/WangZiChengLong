ec43c4b191bcc9a52d69f5b813d2934a8c13f5f7
Dan Li <danli091981@gmail.com>
2024-07-27 22:00:00 -0400

    Update python.vue

--- src/views/compute/python.vue
+++ src/views/compute/python.vue
@@ -2,9 +2,211 @@
     <Navbar />
 
      
-    <div>
-        <h2>python page</h2>
-    </div>
+    <v-container class="my-4">
+        <v-row>
+            <v-col cols="12">
+                <v-img src="../../assets/Python-Header.png" class="my-4" contain height="300" />
+            </v-col>
+        </v-row>
+
+
+        <v-row>
+            <v-col cols="12">
+                <div>
+                    <h2>Python programming</h2>
+
+                    <h3>Why learn python?</h3>
+                    <p>
+                        <ul>
+                            <li><strong>Easy to Learn:</strong> Python's clear and readable syntax makes it beginner-friendly. Compared to other languages with complex structures, Python reads closer to natural language, making it easier to grasp the fundamentals.</li>
+                            <li><strong>Versatile:</strong> Python has a wide range of applications. Whether you're interested in web development, data science, automation, or scientific computing, Python has libraries and frameworks to support your goals.  This makes it a valuable skill for many different career paths.</li>
+                            <li><strong>In-Demand:</strong> Python is a highly sought-after skill in the job market.  Having Python expertise can open doors to exciting career opportunities. With its increasing popularity, Python proficiency is a valuable asset for programmers.</li>
+                        </ul>
+                    </p>
+
+                    <h3>Popular paths</h3>
+                    <p>
+                        <ul>
+                            <li><strong>Web Development:</strong> If you enjoy creating interactive web applications, Python with its web development frameworks like Django and Flask, is a perfect path. You'll build user interfaces, connect to databases, and craft dynamic web experiences.</li>
+                            <li><strong>Data Science and Machine Learning:</strong> Python reigns supreme in data analysis. Libraries like Pandas, NumPy, and Matplotlib empower you to manipulate, visualize, and extract insights from data.  As you progress, delve into Machine Learning with Scikit-learn and TensorFlow to build intelligent systems that can learn and make predictions.</li>
+                            <li><strong>Automation:</strong> Python excels at automating repetitive tasks, saving you time and effort. With libraries like Selenium and PyAutoGUI, you can automate web browsing, interact with desktop applications, and boost your productivity in various aspects of your work.</li>
+                        </ul>
+                    </p>
+
+                    <h3>Books with links</h3>
+                    <ul style="list-style: none;">
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/pythonCrashCourse.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>This book is a fast-paced, thorough introduction to programming with Python that will have you writing programs, solving problems, and developing functioning applications in no time.
+                                             <a href="https://www.amazon.com/Python-Crash-Course-Eric-Matthes-dp-1718502702/dp/1718502702" target="_blank">Python Crash Course, 3rd Edition Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>
+                                </li>
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/autoBoringStuff.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>The second edition of this best-selling Python book uses Python 3 to teach even the technically uninclined how to write programs that do in minutes what would take hours to do by hand. There is no prior programming experience required and the book is loved by liberal arts majors and geeks alike.
+                                            <a href="https://www.amazon.com/Automate-Boring-Stuff-Python-2nd/dp/1593279922" target="_blank">Automate the Boring Stuff with Python, 2nd Edition Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>                                
+                                </li>
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/fluentPython.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>This book goes beyond the basics and teaches you how to write effective, idiomatic Python code by leveraging its best—and possibly most neglected—features.
+                                            <a href="https://www.amazon.com/Fluent-Python-Concise-Effective-Programming/dp/1492056359" target="_blank">Fluent Python, 2nd Edition Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>                                
+                                </li>
+                          
+                            </ul>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <v-container class="my-4">
+        <v-row>
+            <v-col cols="12">
+                <v-img src="../../assets/ml-header.webp" class="my-4" contain height="300" />
+            </v-col>
+        </v-row>
+
+
+        <v-row>
+            <v-col cols="12">
+                <div>
+                    <h2>Machine Learning & Deep Learning</h2>
+
+                    <h3>What is Machine Learning?</h3>
+                    Machine learning (ML) is a branch of artificial intelligence (AI) that focuses on developing algorithms that can learn from data without being explicitly programmed.
+                    <p>
+                        <ul>
+                            <li><strong>Learning from Data:</strong> Unlike traditional programming where you define every step for the computer, machine learning algorithms can learn from patterns in data. This data can be anything from numbers and text to images and videos. The more data the algorithm is exposed to, the better it becomes at identifying patterns and making predictions.</li>
+                            <li><strong>Improving Over Time:</strong> As machine learning algorithms process more data, they improve their performance on a specific task. This is because they can adapt their internal models to better represent the underlying patterns in the data.</li>
+                            <li><strong>Predictions and Decisions:</strong> The ultimate goal of machine learning is to use the learned patterns to make predictions or decisions on new, unseen data. For example, a machine learning algorithm can be trained to identify spam emails, recommend products to customers, or recognize faces in images.</li>
+                        </ul>
+                    </p>
+
+                    <h3>What is Deep Learning?</h3>
+                    Deep learning, a powerful subfield of machine learning, leverages artificial neural networks to solve complex problems. These neural networks are inspired by the structure and function of the human brain.
+                    <p>
+                        <ul>
+                            <li><strong>Artificial Neural Networks:</strong> Inspired by the human brain, deep learning uses artificial neural networks with multiple layers. These layers are interconnected, allowing the network to learn intricate relationships within data.</li>
+                            <li><strong>Learning Through Hierarchies:</strong> Deep learning models process information through these layered networks. Each layer learns a specific level of representation from the data, building progressively complex concepts from simpler ones. Imagine recognizing an animal: the initial layers might detect edges and shapes, while later layers recognize specific features like paws or fur, ultimately classifying the entire image.</li>
+                            <li><strong>Handling Large amounts of Data:</strong> Deep learning excels at handling massive datasets. The complex architecture and ability to learn from vast amounts of data allow deep learning models to identify subtle patterns and make accurate predictions on unseen data.</li>
+                            <li><strong>Widespread Applications:</strong> Deep learning's capabilities power a wide range of applications. It's used in image recognition (facial recognition in photos), speech recognition (voice assistants like Siri), natural language processing (machine translation), and many more. As deep learning continues to evolve, it's expected to play an even greater role in shaping the future of technology.</li>
+                        </ul>
+                    </p>
+
+                    <h3>Books with links</h3>
+                    <ul style="list-style: none;">
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/handson-ML.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>This book is a practical guide that introduces machine learning concepts and techniques using popular Python libraries like Scikit-Learn, Keras, and TensorFlow. It covers various topics like regression, classification, clustering, neural networks, and deep learning.
+                                             <a href="https://www.amazon.in/Hands-Machine-Learning-Scikit-Learn-TensorFlow/dp/9355421982" target="_blank">Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow, 3rd Edition Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>
+                                </li>
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/dpPython.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>This book is a great introduction to deep learning for those with a basic understanding of Python. It covers the fundamental concepts of deep learning, including neural networks, convolutional neural networks (CNNs), recurrent neural networks (RNNs), and more. The book also includes code examples and exercises to help you practice what you learn.
+                                            <a href="https://www.amazon.com/Deep-Learning-Python-Francois-Chollet/dp/1617294438" target="_blank">Deep Learning with Python Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>                                
+                                </li>
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/deepLearning.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>This comprehensive textbook covers the foundations of deep learning, including neural networks, optimization algorithms, and learning theory. It's a great resource for those who want a deep understanding of the theoretical underpinnings of deep learning.
+                                            <a href="https://www.amazon.com/Deep-Learning-Adaptive-Computation-Machine/dp/0262035618" target="_blank">Deep Learning Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>                                
+                                </li>
+                          
+                            </ul>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <v-container class="my-4">
+        <v-row>
+            <v-col cols="12">
+                <v-img src="../../assets/llm_gpt.png" class="my-4" contain height="300" />
+            </v-col>
+        </v-row>
+
+
+        <v-row>
+            <v-col cols="12">
+                <div>
+                    <h2>Generative AI & Large Language Models</h2>
+
+                    <h3>What is Generative AI?</h3>
+                    Generative AI is a revolutionary branch of artificial intelligence that focuses on creating new content, rather than simply analyzing or categorizing existing data.                     
+                    <p>
+                        <ul>
+                            <li><strong>Creative Potential:</strong> Generative AI can produce a wide array of content types, including images, music, videos, and even 3D models, expanding beyond text generation.</li>
+                            <li><strong>Multi-modal Capabilities:</strong> These systems can work across different forms of media, often combining various inputs to create novel outputs.</li>
+                            <li><strong>Adaptive Learning:</strong> Generative AI models can continually improve and adapt their outputs based on feedback and new data, leading to increasingly sophisticated results.</li>
+                            <li><strong>Transformative Impact:</strong> This technology is reshaping industries by automating creative processes, enhancing product design, and enabling personalized content creation at scale.</li>
+                        </ul>
+                    </p>
+
+                    <h3>Introduction to Large Language Models (LLMs)</h3>
+                    Large Language Models (LLMs) are a specific type of generative AI that excels at understanding and generating human-like text.
+                    Large Language Models (LLMs) are advanced AI systems designed to understand and generate human language. 
+                    They utilize deep learning techniques and are trained on extensive datasets to perform a variety of language-related tasks.                    
+                    <p>
+                        <ul>
+                            <li><strong>Transformer Architecture:</strong> LLMs are built on transformer models, which enable efficient processing of language through self-attention mechanisms.</li>
+                            <li><strong>Extensive Training Data:</strong> They are trained on vast amounts of text data, allowing them to learn complex language patterns and context. </li>
+                            <li><strong>Wide Applications:</strong> LLMs are used in various fields, including natural language processing, customer service, healthcare, and software development.</li>
+                            <li><strong>High Parameter Count:</strong> These models contain millions or even billions of parameters, which help them generate coherent and contextually relevant text.</li>
+                        </ul>
+                    </p>
+
+                    <h3>Books with links</h3>
+                    <ul style="list-style: none;">
+                                <li class="body-1">
+                                    <v-row align="center">
+                                        <v-col cols="12" md="2" class="pr-md-2">
+                                            <v-img src="../../assets/generative_dp.jpg" contain height="300"/>
+                                        </v-col>
+                                        <v-col cols="12" md="6" class="pl-md-2">
+                                            <p>This book is a comprehensive guide to the field of generative AI, focusing on deep learning techniques. This book offers both theoretical knowledge and practical implementation of generative models.
+                                             <a href="https://www.amazon.com/Generative-Deep-Learning-Teaching-Machines/dp/1098134184/ref=sr_1_4" target="_blank">Generative Deep Learning: Teaching Machines To Paint, Write, Compose, and Play, 2nd Edition Amazon book</a></p>
+                                        </v-col>
+                                    </v-row>
+                                </li>
+                          
+                            </ul>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
 
     <Footer />
 </template>
