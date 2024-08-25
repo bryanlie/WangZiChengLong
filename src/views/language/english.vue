d6760cf63e91bf7db63f2d42ec118868726dbcc4
Dan Li <danli091981@gmail.com>
2024-08-25 13:29:35 -0400

    Update english.vue

--- src/views/language/english.vue
+++ src/views/language/english.vue
@@ -41,22 +41,26 @@
                                         <li class="body-1">
                                             <v-row align="center">
                                                 <v-col cols="12" md="2" class="pr-md-2">
-                                                    <v-img src="../../assets/peteCat.jpg" contain height="300"/>
+                                                    <a href="https://www.amazon.com/Pete-Cat-Phonics-Box-Mini-Books/dp/0062404520/ref=sr_1_1?sr=8-1" target="_blank">
+                                                        <v-img src="../../assets/peteTheCat.jpg" contain height="300"/>
+                                                    </a>
                                                 </v-col>
                                                 <v-col cols="12" md="6" class="pl-md-2">
                                                     <p>This engaging box set includes 12 small books that focus on different phonics sounds. The stories are silly and fun, and the illustrations are colorful and eye-catching. Children will love following Pete the Cat on his adventures as they learn to sound out words.
-                                                    <a href="https://www.amazon.com/Pete-Childrens-Books-Phonics-Learn/dp/B0768YV2Y7" target="_blank">Pete the Cat Phonics Amazon book</a></p>
+                                                    <a href="https://www.amazon.com/Pete-12-Book-Phonics-First-Read-ebook/dp/B08BLM3MTR/ref=tmm_kin_swatch_0?_encoding=UTF8&sr=8-1" target="_blank">Pete the Cat Phonics, Kindle read for Free</a></p>
                                                 </v-col>
                                             </v-row>
                                         </li>
                                         <li class="body-1">
                                             <v-row align="center">
                                                 <v-col cols="12" md="2" class="pr-md-2">
-                                                    <v-img src="../../assets/fatCatMat.jpg" contain height="300"/>
+                                                    <a href="https://www.amazon.com/Fat-Other-Tales-Phonics-Readers/dp/1474995535/ref=sr_1_1?s=books&sr=1-1" target="_blank">
+                                                        <v-img src="../../assets/fatCatMat.jpg" contain height="300"/>
+                                                    </a>
                                                 </v-col>
                                                 <v-col cols="12" md="6" class="pl-md-2">
                                                     <p>This collection of 12 rhyming stories is a fun way for children to learn about phonics. The stories are short and easy to read, and they focus on different phonics sounds. The book also includes a listen-along CD, which can be helpful for children who are just learning to read.
-                                                    <a href="https://www.amazon.com/other-tales-Phonics-Readers-Collection/dp/1409509230" target="_blank">Fat cat on a mat and other tales Amazon book</a></p>
+                                                </p>
                                                 </v-col>
                                             </v-row>                                
                                         </li>
@@ -111,22 +115,51 @@
                                         <li class="body-1">
                                             <v-row align="center">
                                                 <v-col cols="12" md="2" class="pr-md-2">
-                                                    <v-img src="../../assets/catinHat.jpg" contain height="300"/>
+                                                    <a href="https://www.amazon.com/Cat-Hat-Dr-Seuss/dp/039480001X" target="_blank">
+                                                        <v-img src="../../assets/catinHat.jpg" contain height="300"/>
+                                                    </a>
                                                 </v-col>
-                                                <v-col cols="12" md="6" class="pl-md-2">
-                                                    <p>This is a classic picture book by Dr. Seuss, this silly story features the rambunctious Cat in the Hat and his chaotic visit to two young children on a rainy day.
-                                                    <a href="https://www.amazon.com/Cat-Hat-Dr-Seuss/dp/039480001X" target="_blank">The Cat in the Hat Amazon book</a></p>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Lorax-Classic-Seuss-Dr/dp/0394823370/ref=pd_sim_d_sccl_3_6/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/lorax.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Fox-Socks-Beginner-Books-Seuss/dp/0394800389/ref=pd_sim_d_sccl_3_3/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/foxSocks.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/dp/0394800788" target="_blank">
+                                                        <v-img src="../../assets/hortonHears.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/One-Fish-Blue-Read-Myself/dp/0394800133/ref=pd_sim_d_sccl_3_2/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/onefishBluefish.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/dp/0394800796" target="_blank">
+                                                        <v-img src="../../assets/grinchXmas.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="10" class="pl-md-2">
+                                                    <p>Dr. Seuss, the pen name of Theodor Geisel, is a beloved children's author famous for his whimsical stories and colorful illustrations. His books often feature quirky characters and imaginative worlds, teaching young readers important life lessons in a fun and engaging way. With catchy rhymes and playful language, Dr. Seuss's works have captured the hearts of children and adults alike for generations. His most iconic characters include The Cat in the Hat, Horton the Elephant, and the Grinch.
+                                                    </p>
                                                 </v-col>
                                             </v-row>
                                         </li>
                                         <li class="body-1">
                                             <v-row align="center">
                                                 <v-col cols="12" md="2" class="pr-md-2">
-                                                    <v-img src="../../assets/charlotteWeb.jpg" contain height="300"/>
+                                                    <a href="https://www.amazon.com/Charlottes-Web-Read-Aloud-White/dp/0060882611" target="_blank">
+                                                        <v-img src="../../assets/charlotteWeb.jpg" contain height="300"/>
+                                                    </a>
                                                 </v-col>
                                                 <v-col cols="12" md="6" class="pl-md-2">
                                                     <p>This beloved novel tells the story of Wilbur, a pig, and Charlotte, a spider, who become friends. Charlotte hatches a plan to save Wilbur from slaughter.
-                                                    <a href="https://www.amazon.com/Charlottes-Web-Read-Aloud-White/dp/0060882611" target="_blank">Charlotte's Web Amazon book</a></p>
+                                                    <a href="https://www.amazon.com/Charlottes-Web-E-B-White-audiobook/dp/B0000W6SOK/ref=tmm_aud_swatch_0?_encoding=UTF8&qid=&sr=" target="_blank">Audiobook Free trial</a></p>
                                                 </v-col>
                                             </v-row>                                
                                         </li>
@@ -182,25 +215,48 @@
                                         <li class="body-1">
                                             <v-row align="center">
                                                 <v-col cols="12" md="2" class="pr-md-2">
-                                                    <v-img src="../../assets/sorceresStone.jpg" contain height="300"/>
+                                                    <a href="https://www.amazon.com/gp/product/0590353403" target="_blank">
+                                                        <v-img src="../../assets/sorceresStone.jpg" contain height="300"/>
+                                                    </a>
                                                 </v-col>
-                                                <v-col cols="12" md="6" class="pl-md-2">
-                                                    <p>Eleven-year-old Harry Potter learns on his eleventh birthday that he's the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child by his parents' friend Albus Dumbledore, a powerful sorcerer and headmaster of Hogwarts School of Witchcraft and Wizardry.
-                                                    <a href="https://www.amazon.com/gp/product/0590353403" target="_blank">The Sorcerer's Stone Amazon book</a></p>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Harry-Potter-Chamber-Secrets-Rowling/dp/0439064864/ref=pd_bxgy_thbs_d_sccl_1/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/chamberSecrets.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Harry-Potter-Prisoner-Azkaban-Rowling/dp/0439136350/ref=pd_bxgy_thbs_d_sccl_2/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/prisonerAzkaban.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Harry-Potter-Goblet-Fire-Book/dp/0439139597/ref=bmx_dp_om0pagio_d_sccl_2_1/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/gobletFire.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Harry-Potter-Order-Phoenix-Book/dp/043935806X/ref=bmx_dp_om0pagio_d_sccl_2_5/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/orderPhoenix.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Harry-Potter-Half-Blood-Prince-Book/dp/0439784549/ref=bmx_dp_om0pagio_d_sccl_2_4/142-8678302-4789264" target="_blank">
+                                                        <v-img src="../../assets/halfbloodPrince.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+
+                                                <v-col cols="12" md="2" class="pr-md-2">
+                                                    <a href="https://www.amazon.com/Harry-Potter-Deathly-Hallows-Book/dp/1338878980" target="_blank">
+                                                        <v-img src="../../assets/deathlyHallows.jpg" contain height="300"/>
+                                                    </a>
+                                                </v-col>
+
+                                                <v-col cols="12" md="8" class="pl-md-2">
+                                                    <p>The Harry Potter series is a beloved fantasy series that follows the adventures of a young wizard named Harry Potter. Harry discovers he is a wizard and attends Hogwarts School of Witchcraft and Wizardry, where he learns about magic and confronts the dark wizard Lord Voldemort. The series explores themes of friendship, loyalty, and the power of good over evil. With its rich characters, imaginative world, and thrilling plot, Harry Potter has captivated readers of all ages around the globe.
+                                                   </p>
                                                 </v-col>
                                             </v-row>
-                                        </li>
-                                        <li class="body-1">
-                                            <v-row align="center">
-                                                <v-col cols="12" md="2" class="pr-md-2">
-                                                    <v-img src="../../assets/deathlyHallows.jpg" contain height="300"/>
-                                                </v-col>
-                                                <v-col cols="12" md="6" class="pl-md-2">
-                                                    <p>Harry, Ron, and Hermione go on a perilous mission to find and destroy Voldemort's Horcruxes, the source of his immortality. The fate of the wizarding world hangs in the balance as they face their ultimate battle.
-                                                    <a href="https://www.amazon.com/Harry-Potter-Deathly-Hallows-Book/dp/1338878980" target="_blank">The Deathly Hallows Amazon book</a></p>
-                                                </v-col>
-                                            </v-row>                                
-                                        </li>
+                                        </li>           
                                     </ul>
                         </div>
                     </v-col>
