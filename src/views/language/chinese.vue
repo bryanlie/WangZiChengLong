e36715150c95036f7f09634bd4c152b149d70051
Dan Li <danli091981@gmail.com>
2024-08-25 13:29:02 -0400

    Update chinese.vue

--- src/views/language/chinese.vue
+++ src/views/language/chinese.vue
@@ -30,7 +30,7 @@
                                 <ul>
                                     <li>LingoAce is an online language learning platform that specializes in teaching Mandarin Chinese to children aged 3-15 years old. 
                                         LingoAce offers one-on-one online classes with dedicated teachers tailored to each child's unique learning needs and goals.
-                                        <a href="https://www.lingoace.com/programs/learn-chinese/" target="_blank">LingoAce Learn Chinese</a></li>
+                                        <a href="https://r.lingoace.com/iv9GHoP1WvEA" target="_blank">FREE trial class @LingoAce</a></li>
                                     <li>Yoyo Chinese is an online platform designed to teach Mandarin Chinese, particularly for beginners and intermediate learners.
                                         The platform uses short video lessons (typically 4-10 minutes long) to explain concepts, pronunciations, and cultural information.
                                         <a href="https://yoyochinese.com/" target="_blank">Yoyo Chinese</a></li>
@@ -55,6 +55,99 @@
                                     </li>
                                 </ul>
                             </p>
+
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-img src="../../assets/4classics.jpg" class="my-4" contain height="300" />
+                                </v-col>
+                            </v-row>
+
+                            <h3>The Four Chinese Classic Novels</h3>
+                            <p>
+                                <ul style="list-style: none;">
+                                    <li class="body-1">
+                                        <v-row align="center">
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Journey-West-Revised-1/dp/0226971325/ref=sr_1_1" target="_blank">
+                                                    <v-img src="../../assets/xiyouV1.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Journey-West-Revised-2/dp/0226971341/ref=pd_bxgy_thbs_d_sccl_1/142-8678302-4789264" target="_blank">
+                                                    <v-img src="../../assets/xiyouV2.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Journey-West-Revised-3/dp/0226971376/ref=pd_bxgy_d_sccl_2/142-8678302-4789264" target="_blank">
+                                                    <v-img src="../../assets/xiyouV3.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Journey-West-Revised-4/dp/0226971392/ref=pd_bxgy_d_sccl_2/142-8678302-4789264" target="_blank">
+                                                    <v-img src="../../assets/xiyouV4.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="10" class="pl-md-2">
+                                                <p>Journey to the West is a classic Chinese novel that follows the journey of a monk and his disciples as they travel to the West to retrieve sacred scriptures. The central figure is Sun Wukong, the Monkey King, a powerful and mischievous deity with incredible abilities. Along the way, they face a myriad of challenges, encountering a colorful cast of characters and learning valuable lessons about morality, perseverance, and faith. Journey to the West is not only a thrilling adventure story but also a profound exploration of Chinese culture, philosophy, and religion.
+                                                </p>
+                                            </v-col>
+                                        </v-row>
+                                    </li>
+                                    
+                                    <li class="body-1">
+                                        <v-row align="center">
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Story-Stone-Dream-Chamber-Vol/dp/0140442936/ref=sr_1_1" target="_blank">
+                                                    <v-img src="../../assets/honglouV1.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Story-Stone-Vol-Crab-Flower-Club/dp/0140443266/ref=pd_bxgy_thbs_d_sccl_1/142-8678302-4789264" target="_blank">
+                                                    <v-img src="../../assets/honglouV2.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Story-Stone-Dream-Chamber-Warning/dp/0140443703/ref=pd_bxgy_thbs_d_sccl_1/142-8678302-4789264" target="_blank">
+                                                    <v-img src="../../assets/honglouV3.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+            
+                                            <v-col cols="12" md="10" class="pl-md-2">
+                                                <p>Dream of the Red Chamber is a complex and poignant Chinese novel that explores themes of love, loss, and the decline of a wealthy family. The story follows Jia Baoyu, a young man who finds himself caught between the demands of his family and his own desires. Through intricate plotlines and psychologically rich characters, the novel delves into the complexities of human relationships, the fleeting nature of happiness, and the inevitable passage of time.
+                                                </p>
+                                            </v-col>
+                                        </v-row>
+                                    </li>
+                                    <li class="body-1">
+                                        <v-row align="center">
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Romance-Three-Kingdoms-Luo-Guanzhong/dp/024133277X/ref=sr_1_1" target="_blank">
+                                                    <v-img src="../../assets/sanguo.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="8" class="pl-md-2">
+                                                <p>Romance of the Three Kingdoms is an epic Chinese historical novel that chronicles the turbulent years of the late Han dynasty and the subsequent Three Kingdoms period (220-280 AD). Based on the real-life events and figures of this era, the novel offers a captivating blend of history, fiction, and moral philosophy. The story revolves around three primary figures: Liu Bei, a righteous and benevolent leader; Cao Cao, a cunning and ambitious strategist; and Sun Quan, a young and ambitious warlord. The novel explores themes of power, loyalty, and betrayal, while offering insights into Chinese culture and philosophy.
+                                                </p>
+                                            </v-col>
+                                        </v-row>
+                                    </li>
+                                    <li class="body-1">
+                                        <v-row align="center">
+                                            <v-col cols="12" md="2" class="pr-md-2">
+                                                <a href="https://www.amazon.com/Water-Margin-Outlaws-Classic-Classics/dp/0804840954/ref=tmm_pap_swatch_0" target="_blank">
+                                                    <v-img src="../../assets/shuihu.jpg" contain height="300"/>
+                                                </a>
+                                            </v-col>
+                                            <v-col cols="12" md="8" class="pl-md-2">
+                                                <p>Water Margin is an epic Chinese novel that tells the story of a group of outlaws who gather together at Liangshan Marsh to rebel against the corrupt government of the Northern Song dynasty. The novel is renowned for its vivid characters, each with unique personalities and skills. From the righteous Song Jiang to the cunning Wu Song, the outlaws' adventures are filled with exciting action sequences, daring escapes, and moral dilemmas. Water Margin offers a fascinating exploration of Chinese society, morality, and the complexities of human nature.
+                                                </p>
+                                            </v-col>
+                                        </v-row>
+                                    </li>
+                                </ul>
+                    
+                            </p>
+
                         </div>
                     </v-col>
                 </v-row>
