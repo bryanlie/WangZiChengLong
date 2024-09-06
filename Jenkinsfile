eea940da1659d5dfbe7939128fcdf307a315660a
bryanlie <danli091981@gmail.com>
2024-09-06 00:14:58 -0400

    add locales on homepage

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,45 +2,57 @@ pipeline {
     agent any
 
     environment {
-        AWS_ACCOUNT_ID=${{ secrets.AWS_ACCOUNT_ID }}
+        AWS_ACCOUNT_ID=credentials('aws-account-id')
         AWS_DEFAULT_REGION="us-east-1" 
         IMAGE_REPO_NAME="WangZiChengLong"
         IMAGE_TAG="${env.BUILD_NUMBER}"
-        REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}"
     }
 
     stages {    
         stage('Checkout') {
             steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/bryanlie/WangZiChengLong.git']])
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/main']], 
+                    userRemoteConfigs: [[
+                        url: 'https://github.com/bryanlie/WangZiChengLong.git',
+                        credentialsId: 'github-access-token'
+                    ]]
+                ])
             }
         }
         stage('Build') {
             steps {
-                sh 'npm install'
-                sh 'npm run build'
+                bat 'npm install'
+                bat 'npm run build'
             }
         }
         stage('Build Docker Image') {
             steps {
                 script {
-                    dockerImage = docker.build "${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+                    def lowerCaseRepoName = IMAGE_REPO_NAME.toLowerCase()
+                    dockerImage = docker.build "${lowerCaseRepoName}:${IMAGE_TAG}"
                 }
             }
         }
         stage('Push to ECR') {
             steps {
                 script {
-                    sh "aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-                    sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG"
-                    sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+                    REPOSITORY_URI = "${env.AWS_ACCOUNT_ID}.dkr.ecr.${env.AWS_DEFAULT_REGION}.amazonaws.com/${env.IMAGE_REPO_NAME}"
+                    withCredentials([string(credentialsId: 'aws-account-id', variable: 'AWS_ACCOUNT_ID')]) {
+                        bat """
+                            aws ecr get-login-password --region ${env.AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
+                            docker tag ${env.IMAGE_REPO_NAME}:${env.IMAGE_TAG} ${REPOSITORY_URI}:${env.IMAGE_TAG}
+                            docker push ${REPOSITORY_URI}:${env.IMAGE_TAG}
+                        """
+                    }
                 }
             }
         }        
         stage('Deploy to ECS') {
             steps {
-                withAWS(credentials: ${{ secrets.AWS_CREDENTIALS_ID }}, region: "${AWS_DEFAULT_REGION}") {
-                    sh 'aws ecs update-service --cluster web-cluster --service my-nginx-service --force-new-deployment'
+                withAWS(credentials: credentials('aws-jenkins-credentials'), region: "${env.AWS_DEFAULT_REGION}") {
+                    bat 'aws ecs update-service --cluster web-cluster --service my-nginx-service --force-new-deployment'
                 }
             }
         }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -50,28 +50,22 @@
   
       
   <v-container fluid class="ma-5">
-      <h2>Bridging East and West: A Blended Approach to Education</h2>
-      <p>As we embark on this journey together, it's important to acknowledge the distinct approaches to education in
-         Eastern and Western cultures. Each offers unique strengths, and by understanding these differences, 
-         we can create a more holistic learning experience for our children.</p>
+      <h2>{{ $t('homepage.bridge')}}</h2>
+      <p>{{ $t('homepage.embark') }}</p>
 
         <v-row>
         <v-col cols="12" md="6">
           <v-card class="pa-4">
             <v-card-text>
       
-              <p><strong>Eastern Education</strong> emphasizes a strong foundation in core subjects like math, science, and language, 
-                building a solid base for future learning. This focus on mastery is complemented by a culture of respect for teachers and elders, 
-                fostering discipline and a strong work ethic. Eastern classrooms often utilize memorization techniques to promote knowledge retention and focus.</p>
+              <p><strong>{{ $t('homepage.east_edu') }}  </strong> {{ $t('homepage.key_pt3') }}</p>
             </v-card-text>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
           <v-card class="pa-4">
             <v-card-text>
-              <p><strong>Western Education</strong> fosters well-rounded individuals by encouraging critical thinking, analysis, and problem-solving skills. 
-                 Western classrooms celebrate individuality and self-expression, 
-                allowing students to share their unique perspectives. Collaborative learning is also a hallmark, preparing students for teamwork and success in the real world.</p>
+              <p><strong>{{ $t('homepage.west_edu') }}</strong> {{ $t('homepage.foster') }}</p>
             </v-card-text>
           </v-card>
         </v-col>
@@ -81,12 +75,12 @@
         <v-col cols="20" sm="12">
           <v-card class="pa-4">
             <v-card-text>
-                <h2>Creating a Holistic Learning Experience by Embracing the Strengths of Both Worlds</h2>
-              <p>By acknowledging these differences, we can strive to create a blended approach that leverages the strengths of both Eastern and Western education. Here's how:</p>
+                <h2>{{ $t('homepage.experience')}}</h2>
+              <p>{{ $t('homepage.difference')}}</p>
               <ul>
-                <li>Balance Rigor with Innovation: We can incorporate the emphasis on foundational knowledge from the East with the encouragement of creativity from the West.</li>
-                <li>Foster Respectful Inquiry: Encourage students to ask questions while maintaining an environment that honors teachers and the learning process.</li>
-                <li>Develop Well-Rounded Individuals: Combine a focus on core subjects with an appreciation for diverse skills and interests.</li>
+                <li>{{ $t('homepage.innovation') }}</li>
+                <li>{{ $t('homepage.inquiry')}}</li>
+                <li>{{ $t('homepage.individual')}}</li>
               </ul>
             </v-card-text>
           </v-card>
@@ -100,10 +94,8 @@
                     </a>  
                   </v-col>
                   <v-col cols="12" md="6" class="pl-md-2">
-                      <p>Battle Hymn of the Tiger Mother is a memoir by Amy Chua that ignited a global debate on parenting styles. 
-                        Chua candidly shares her experiences raising her daughters with a demanding, Chinese-influenced approach she terms "Tiger Mothering." 
-                        By contrasting her methods with the perceived permissiveness of Western parenting, the book sparked intense discussions about culture, child development, and the appropriate balance between nurture and discipline. 
-                        <a href="https://www.amazon.com/Battle-Hymn-of-Tiger-Mother-audiobook/dp/B004INSU48/ref=tmm_aud_swatch_0?_encoding=UTF8&qid=&sr=" target="_blank">Audiobook Free trial</a>
+                      <p>{{ $t('homepage.tiger_mom')}}
+                        <a href="https://www.amazon.com/Battle-Hymn-of-Tiger-Mother-audiobook/dp/B004INSU48/ref=tmm_aud_swatch_0?_encoding=UTF8&qid=&sr=" target="_blank">{{ $t('homepage.audiobook')}}</a>
                       </p>
                   </v-col>
               </v-row>
@@ -113,20 +105,16 @@
 
     
   <v-container fluid class="ma-5">
-      <h2>Cracking the Ivy League Code: A Guide to College Admissions Success</h2>
-      <p>Unlocking the gatekeepers to top universities, including Ivy League institutions, is a dream for many students. 
-        These institutions are renowned for their academic rigor and exceptional learning environments. 
-        Here, we'll unveil some general principles and guidelines to strengthen your child's application and increase their chances of admission.
+      <h2>{{ $t('homepage.crack')}}</h2>
+      <p>{{ $t('homepage.unlock')}}
       </p>
 
       <v-row>
         <v-col cols="20" sm="12">
           <v-card class="pa-4">
             <v-card-text>
-              <p><strong>Academic Excellence:</strong> Strong academic performance is a cornerstone of college admissions. Encourage your child to excel in their coursework, 
-                take challenging classes, and maintain a high GPA. Additionally, standardized test scores like the SAT or ACT play a significant role in the admissions process.</p>
-              <p><strong>Extracurricular Involvement:</strong> in extracurricular activities demonstrates a well-rounded student. Encourage your child to pursue their passions, 
-                join clubs, volunteer, and take on leadership roles. These experiences showcase their interests, skills, and commitment beyond the classroom.</p>
+              <p><strong>{{ $t('homepage.academic')}}</strong> {{ $t('homepage.key_pt4')}}</p>
+              <p><strong>{{ $t('homepage.extra')}}</strong> {{ $t('homepage.activity') }} </p>
             </v-card-text>
           </v-card>
           
@@ -151,22 +139,16 @@
     </v-container>
       
     <v-container fluid class="ma-5">
-    <h2>Unlocking Your Child's Potential: Nurturing Lifelong Learning</h2>
-    <p>While college admissions are a significant milestone, the journey doesn't end there. Lifelong learning is a continuous process that extends far beyond the classroom, 
-      enriching your child's personal and professional life. Here are some key aspects of lifelong education:</p>
+      <h2>{{ $t('homepage.title') }}</h2>
+      <p>{{ $t('homepage.description') }}</p>
     <v-row>
       <v-col cols="20" sm="12">
         <v-card class="pa-4">
           <v-card-text>
-            <p><strong>Developing Life Skills:</strong> 
-              Beyond academics, lifelong education encompasses essential life skills like financial literacy, relationship management, 
-              communication skills, and critical thinking. These skills empower individuals to navigate complex situations, make sound decisions, and build fulfilling lives.</p>
-            <p><strong>Adaptability and Resilience: </strong> 
-              world is constantly changing, and lifelong learning fosters the ability to adapt to new information, technologies, 
-              and career landscapes. By encouraging a love of learning, you equip your child with the resilience and flexibility to thrive in an ever-evolving world.</p>
-            <p><strong>Personal Growth and Well-Being:</strong> 
-              Lifelong learning isn't just about acquiring knowledge; it's about personal growth and well-being. 
-              Exploring diverse interests, hobbies, and learning opportunities fosters self-discovery, creativity, and a sense of purpose.</p>
+            <p><strong>{{ $t('homepage.life_skills_title') }}</strong> {{ $t('homepage.life_skills_desc') }}</p>
+            <p><strong>{{ $t('homepage.adaptability_title') }}</strong> {{ $t('homepage.adaptability_desc') }}</p>
+            <p><strong>{{ $t('homepage.personal_growth_title') }}</strong> {{ $t('homepage.personal_growth_desc') }}</p>
+
           </v-card-text>
         </v-card>
       </v-col>
